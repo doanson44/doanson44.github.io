b0e95fd5e3ce12f86d0d69236e4fb7bca8584833
Son Thai <doanson44@gmail.com>
2026-09-22 10:55:24 +0700

    style: apply rustfmt-compatible formatting

--- src/features/cv/page.rs
+++ src/features/cv/page.rs
@@ -2,8 +2,8 @@ use leptos::prelude::*;
 
 use super::components::{CvSection, EducationCard, ExperienceCard, SkillGroup};
 use super::data::{competencies, education, experiences, highlights, profile, skill_categories};
-use crate::infrastructure::browser::print_page;
 use crate::i18n::*;
+use crate::infrastructure::browser::print_page;
 
 /// Public CV and technical portfolio page.
 #[component]
