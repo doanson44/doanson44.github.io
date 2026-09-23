c60b202088265fe87065224ce7a4b3d47be72f41
Son Thai <doanson44@gmail.com>
2026-09-23 17:28:43 +0700

    fix: localize socket analysis column

--- src/features/socket/page.rs
+++ src/features/socket/page.rs
@@ -128,7 +128,7 @@ pub fn SocketPage(
                                         <th class="px-3 py-2 text-right font-medium" scope="col">
                                             <SortHeader state=state mode=SocketSortMode::TotalTicks align="right">{move || t_string!(i18n, socket_activity)} </SortHeader>
                                         </th>
-                                        <th class="px-3 py-2 text-right font-medium" scope="col">"Analysis"</th>
+                                        <th class="px-3 py-2 text-right font-medium" scope="col">{move || t_string!(i18n, socket_analysis)}</th>
                                     </tr>
                                 </thead>
                                 <tbody>
