911ddd8d6b5e5939c8e3741257db58cb7c3cee98
Son Thai <doanson44@gmail.com>
2026-09-23 10:33:13 +0700

    feat: add CafeF market tool

--- src/app.rs
+++ src/app.rs
@@ -18,6 +18,7 @@ use crate::features::tools::finance::page::FinancePage;
 use crate::features::tools::json::page::JsonPage;
 use crate::features::tools::jwt::page::JwtPage;
 use crate::features::tools::markdown::page::MarkdownPage;
+use crate::features::tools::market::page::MarketPage;
 use crate::features::tools::page::ToolsPage;
 use crate::features::tools::proxy::page::ProxyPage;
 use crate::features::tools::time::page::TimePage;
@@ -93,6 +94,7 @@ fn render_page(route: String) -> leptos::prelude::AnyView {
         "/tools/jwt" => view! { <JwtPage /> }.into_any(),
         "/tools/base64" => view! { <Base64Page /> }.into_any(),
         "/tools/time" => view! { <TimePage /> }.into_any(),
+        "/tools/market" => view! { <MarketPage /> }.into_any(),
         "/tools/proxy" => view! { <ProxyPage /> }.into_any(),
         "/games" => view! { <GamesPage game=None /> }.into_any(),
         "/cv" => view! { <CvPage /> }.into_any(),
