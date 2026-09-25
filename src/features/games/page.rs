9af0faf97a0de30aa9bec8c50dc7e98fe768ed84
Son Thai <doanson44@gmail.com>
2026-09-25 11:08:33 +0700

    style: format game imports

--- src/features/games/page.rs
+++ src/features/games/page.rs
@@ -6,9 +6,10 @@ use crate::domain::games::{
     connect_four_ai_column, connect_four_drop, connect_four_winner, hangman_word, has_move_2048,
     lights_toggle, minesweeper_adjacent_mines_sized, minesweeper_flood_reveal_sized,
     puzzle_is_solved, puzzle_move, shuffle_deck, slide_2048, snake_step, sudoku_given,
-    sudoku_puzzle_with_seed, sudoku_valid, tetris_clear_filled, tetris_rotate_cw, tower_wave_countdown,
-    tower_wave_damage, ttt_best_move_sized, ttt_is_draw_sized, ttt_winner_sized, typing_words,
-    wordle_check, wordle_word, BreakoutGame, BreakoutTickResult, FlappyGame, PongGame,
+    sudoku_puzzle_with_seed, sudoku_valid, tetris_clear_filled, tetris_rotate_cw,
+    tower_wave_countdown, tower_wave_damage, ttt_best_move_sized, ttt_is_draw_sized,
+    ttt_winner_sized, typing_words, wordle_check, wordle_word, BreakoutGame, BreakoutTickResult,
+    FlappyGame, PongGame,
 };
 use leptos::ev;
 use leptos::prelude::*;
