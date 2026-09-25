72a685050c14d7bcf33f9139b5b61017048ac557
Son Thai <doanson44@gmail.com>
2026-09-25 16:54:01 +0700

    style(games): apply rustfmt import formatting

--- src/features/games/page.rs
+++ src/features/games/page.rs
@@ -3,10 +3,10 @@ use crate::application::services::games::{BreakoutService, PongService};
 use crate::domain::games::{
     connect_four_ai_column, connect_four_drop, connect_four_winner, hangman_word, has_move_2048,
     lights_toggle, minesweeper_adjacent_mines_sized, minesweeper_flood_reveal_sized,
-    puzzle_is_solved, puzzle_move, slide_2048, snake_step, sudoku_given,
-    sudoku_puzzle_with_seed, sudoku_valid, tetris_clear_filled, tetris_rotate_cw,
-    ttt_best_move_sized, ttt_is_draw_sized, ttt_winner_sized, typing_reactor_tasks, wordle_check,
-    wordle_word, BreakoutGame, BreakoutTickResult, FlappyGame, PongGame, TypingReactor,
+    puzzle_is_solved, puzzle_move, slide_2048, snake_step, sudoku_given, sudoku_puzzle_with_seed,
+    sudoku_valid, tetris_clear_filled, tetris_rotate_cw, ttt_best_move_sized, ttt_is_draw_sized,
+    ttt_winner_sized, typing_reactor_tasks, wordle_check, wordle_word, BreakoutGame,
+    BreakoutTickResult, FlappyGame, PongGame, TypingReactor,
 };
 use leptos::ev;
 use leptos::prelude::*;
